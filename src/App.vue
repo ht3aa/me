aa180929d61c0713c38b34593374f21872026430
ht3aa <ht3aa2001@gmail.com>
2023-05-12 14:17:41 +0300

    adding lightening component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup>
-import HeaderVue from "./components/Header.vue";
+import Lightening from "./components/Lightening.vue";
 </script>
 
 <template>
-  <HeaderVue />
+  <Lightening />
+  <p class="w-[200px] h-[200px] bg-black">hello mother father</p>
 </template>
+
+<style>
+@font-face {
+  font-family: dubai;
+  src: url("@/assets/fonts/dubai/DubaiW23-Regular.ttf");
+}
+
+body {
+  font-family: dubai;
+}
+</style>
---
+++ src/components/Lightening.vue
@@ -0,0 +1,51 @@
+<script setup>
+const lighteningImg = ref(null);
+import { onMounted, ref } from "vue";
+
+const generateBurnMark = (x, y) => {
+  let img = document.createElement("img");
+  img.setAttribute("src", "src/assets/imgs/burnMark.png");
+  img.setAttribute(
+    "style",
+    `left: ${x + 53}px; top:${y + -49}px; position: absolute; width: 20px;`
+  );
+  document.body.append(img);
+};
+
+onMounted(() => {
+  let x = 0;
+  let y = 0;
+  document.addEventListener("mousemove", (e) => {
+    x = e.clientX;
+    y = e.clientY;
+
+    lighteningImg.value.style.left = x + 17 + "px";
+    lighteningImg.value.style.top = y + -150 + "px";
+  });
+
+  document.addEventListener("click", () => {
+    lighteningImg.value.setAttribute("src", "src/assets/imgs/lightening.gif");
+
+    setTimeout(() => {
+      generateBurnMark(x, y);
+      lighteningImg.value.setAttribute("src", "src/assets/imgs/lightening.png");
+    }, 1000);
+  });
+});
+</script>
+
+<template>
+  <img
+    ref="lighteningImg"
+    class="lighteningImg w-[120px] h-[120px] absolute"
+    style="left: -100px"
+    src="@/assets/imgs/lightening.png"
+    alt="lightening"
+  />
+</template>
+
+<style scoped>
+.lighteningImg {
+  transition: all 0.1s linear;
+}
+</style>
