283d1c3af0215a77cae477a24906a2cbd363d3b0
ht3aa <ht3aa2001@gmail.com>
2023-09-12 13:28:29 +0300

    refactor projects section by adding v-for

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,7 +5,7 @@ import MovingCard from "../components/cards/MovingCard.vue";
 import LazyImg from "../components/LazyImg.vue";
 import BarChart from "../components/charts/BarChart.vue";
 import Doaa from "../components/religion/Doaa.vue";
-import { customerOpinions } from "../utils/global";
+import { customerOpinions, projects } from "../utils/global";
 import { ref } from "vue";
 
 
@@ -99,158 +99,155 @@ const showLessOpinions = () => {
     id="projects"
     class="wrapper grid gap-10 grid-cols-1 md:grid-cols-2 my-5"
   >
-    <RotateCard>
-      <a href="#hero">
+    <RotateCard v-for="project in projects" :key="project.id">
+      <a :href="project.href">
         <div class="flex h-full">
           <LazyImg
             extra-div-classes="md:w-1/3 w-[30%]"
             extra-img-classes="object-cover h-full aspect-[0.6]"
             extra-blurred-img-classes="object-cover h-full aspect-[0.6]"
-            blurred-img-src="/imgs/me-small.png"
-            main-img-src="/imgs/me.png"
-            alt="portfolio"
+            :blurred-img-src="project.blurredImg"
+            :main-img-src="project.mainImg"
+            :alt="project.imgAlt"
           />
           <div class="w-10/12 py-3 px-5 flex flex-col justify-center relative">
             <div
               class="w-[3px] h-[90%] bgMainColor absolute left-1 top-[50%] translate-y-[-50%]"
             />
-            <h3 class="text-2xl">Portfolio</h3>
-            <p class="text-md">
-              Contains information about me, big projects that I has done, and
-              what I offer to you as a friend first and customer second.
-            </p>
+            <h3 class="text-2xl">{{ project.title }}</h3>
+            <p class="text-md">{{ project.description }}</p>
           </div>
         </div>
       </a>
     </RotateCard>
-    <RotateCard>
-      <a href="https://matlab-bag.vercel.app/">
-        <div class="flex h-full">
-          <LazyImg
-            extra-div-classes="md:w-1/3 w-[30%]"
-            extra-img-classes="object-cover h-full aspect-[0.6]"
-            extra-blurred-img-classes="object-cover h-full aspect-[0.6]"
-            blurred-img-src="/imgs/ali_falih-small.png"
-            main-img-src="/imgs/ali_falih.png"
-            alt="Ali Falih"
-          />
-          <div class="w-10/12 py-3 px-5 flex flex-col justify-center relative">
-            <div
-              class="w-[3px] h-[90%] bgMainColor absolute left-1 top-[50%] translate-y-[-50%]"
-            />
-            <h3 class="text-2xl">Matlab Bag</h3>
-            <p class="text-md">
-              A website that contains files and videos which explain Matlab
-              software easily and simply. Tools that I used: Nuxt.js React
-              framework and Vercel For hosting
-            </p>
-          </div>
-        </div>
-      </a>
-    </RotateCard>
+    <!-- <RotateCard> -->
+    <!--   <a href="https://matlab-bag.vercel.app/"> -->
+    <!--     <div class="flex h-full"> -->
+    <!--       <LazyImg -->
+    <!--         extra-div-classes="md:w-1/3 w-[30%]" -->
+    <!--         extra-img-classes="object-cover h-full aspect-[0.6]" -->
+    <!--         extra-blurred-img-classes="object-cover h-full aspect-[0.6]" -->
+    <!--         blurred-img-src="/imgs/ali_falih-small.png" -->
+    <!--         main-img-src="/imgs/ali_falih.png" -->
+    <!--         alt="Ali Falih" -->
+    <!--       /> -->
+    <!--       <div class="w-10/12 py-3 px-5 flex flex-col justify-center relative"> -->
+    <!--         <div -->
+    <!--           class="w-[3px] h-[90%] bgMainColor absolute left-1 top-[50%] translate-y-[-50%]" -->
+    <!--         /> -->
+    <!--         <h3 class="text-2xl">Matlab Bag</h3> -->
+    <!--         <p class="text-md"> -->
+    <!--           A website that contains files and videos which explain Matlab -->
+    <!--           software easily and simply. Tools that I used: Nuxt.js React -->
+    <!--           framework and Vercel For hosting -->
+    <!--         </p> -->
+    <!--       </div> -->
+    <!--     </div> -->
+    <!--   </a> -->
+    <!-- </RotateCard> -->
 
-    <RotateCard>
-      <a href="https://excel-bag.vercel.app/  ">
-        <div class="flex h-full">
-          <LazyImg
-            extra-div-classes="md:w-1/3 w-[30%]"
-            extra-img-classes="object-cover h-full aspect-[0.6]"
-            extra-blurred-img-classes="object-cover h-full aspect-[0.6]"
-            blurred-img-src="/imgs/mohammed_salman-small.jpg"
-            main-img-src="/imgs/mohammed_salman.jpg"
-            alt="Mohammed Salman"
-          />
-          <div class="w-10/12 py-3 px-5 flex flex-col justify-center relative">
-            <div
-              class="w-[3px] h-[90%] bgMainColor absolute left-1 top-[50%] translate-y-[-50%]"
-            />
-            <h3 class="text-2xl">Excel Bag</h3>
-            <p class="text-md">
-              A website that contains files and videos which explain Excel
-              software easily and simply. Tools that I used: Nuxt.js React
-              framework and Vercel For hosting
-            </p>
-          </div>
-        </div>
-      </a>
-    </RotateCard>
-    <RotateCard>
-      <a href="https://github.com/ht3aa/english_bag">
-        <div class="flex h-full">
-          <LazyImg
-            extra-div-classes="md:w-1/3 w-[30%]"
-            extra-img-classes="object-cover h-full aspect-[0.6]"
-            extra-blurred-img-classes="object-cover h-full aspect-[0.6]"
-            blurred-img-src="/imgs/computer_teqniques_logo-small.png"
-            main-img-src="/imgs/computer_teqniques_logo.png"
-            alt="University Logo"
-          />
-          <div class="w-10/12 py-3 px-5 flex flex-col justify-center relative">
-            <div
-              class="w-[3px] h-[90%] bgMainColor absolute left-1 top-[50%] translate-y-[-50%]"
-            />
-            <h3 class="text-2xl">English Bag</h3>
-            <p class="text-md">
-              A project that contains files and videos which explain English
-              language easily and simply. features: Downloadable lecture files,
-              voice records. You can read the lecture while listening the voice
-              record or the videos responsive.
-            </p>
-          </div>
-        </div>
-      </a>
-    </RotateCard>
+    <!-- <RotateCard> -->
+    <!--   <a href="https://excel-bag.vercel.app/"> -->
+    <!--     <div class="flex h-full"> -->
+    <!--       <LazyImg -->
+    <!--         extra-div-classes="md:w-1/3 w-[30%]" -->
+    <!--         extra-img-classes="object-cover h-full aspect-[0.6]" -->
+    <!--         extra-blurred-img-classes="object-cover h-full aspect-[0.6]" -->
+    <!--         blurred-img-src="/imgs/mohammed_salman-small.jpg" -->
+    <!--         main-img-src="/imgs/mohammed_salman.jpg" -->
+    <!--         alt="Mohammed Salman" -->
+    <!--       /> -->
+    <!--       <div class="w-10/12 py-3 px-5 flex flex-col justify-center relative"> -->
+    <!--         <div -->
+    <!--           class="w-[3px] h-[90%] bgMainColor absolute left-1 top-[50%] translate-y-[-50%]" -->
+    <!--         /> -->
+    <!--         <h3 class="text-2xl">Excel Bag</h3> -->
+    <!--         <p class="text-md"> -->
+    <!--           A website that contains files and videos which explain Excel -->
+    <!--           software easily and simply. Tools that I used: Nuxt.js React -->
+    <!--           framework and Vercel For hosting -->
+    <!--         </p> -->
+    <!--       </div> -->
+    <!--     </div> -->
+    <!--   </a> -->
+    <!-- </RotateCard> -->
+    <!-- <RotateCard> -->
+    <!--   <a href="https://github.com/ht3aa/english_bag"> -->
+    <!--     <div class="flex h-full"> -->
+    <!--       <LazyImg -->
+    <!--         extra-div-classes="md:w-1/3 w-[30%]" -->
+    <!--         extra-img-classes="object-cover h-full aspect-[0.6]" -->
+    <!--         extra-blurred-img-classes="object-cover h-full aspect-[0.6]" -->
+    <!--         blurred-img-src="/imgs/computer_teqniques_logo-small.png" -->
+    <!--         main-img-src="/imgs/computer_teqniques_logo.png" -->
+    <!--         alt="University Logo" -->
+    <!--       /> -->
+    <!--       <div class="w-10/12 py-3 px-5 flex flex-col justify-center relative"> -->
+    <!--         <div -->
+    <!--           class="w-[3px] h-[90%] bgMainColor absolute left-1 top-[50%] translate-y-[-50%]" -->
+    <!--         /> -->
+    <!--         <h3 class="text-2xl">English Bag</h3> -->
+    <!--         <p class="text-md"> -->
+    <!--           A project that contains files and videos which explain English -->
+    <!--           language easily and simply. features: Downloadable lecture files, -->
+    <!--           voice records. You can read the lecture while listening the voice -->
+    <!--           record or the videos responsive. -->
+    <!--         </p> -->
+    <!--       </div> -->
+    <!--     </div> -->
+    <!--   </a> -->
+    <!-- </RotateCard> -->
 
-    <RotateCard>
-      <a href="https://github.com/ht3aa/covid19">
-        <div class="flex h-full">
-          <LazyImg
-            extra-div-classes="md:w-1/3 w-[30%]"
-            extra-img-classes="object-cover h-full aspect-[0.6]"
-            extra-blurred-img-classes="object-cover h-full aspect-[0.6]"
-            blurred-img-src="/imgs/covid19-small.png"
-            main-img-src="/imgs/covid19.png"
-            alt="Covid 19"
-          />
-          <div class="w-10/12 py-3 px-5 flex flex-col justify-center relative">
-            <div
-              class="w-[3px] h-[90%] bgMainColor absolute left-1 top-[50%] translate-y-[-50%]"
-            />
-            <h3 class="text-2xl">Covid 19</h3>
-            <p class="text-md">
-              A project that contains a description about covid-19. also it
-              contains the feature of finding the nearest vaccine from you.
-            </p>
-          </div>
-        </div>
-      </a>
-    </RotateCard>
+    <!-- <RotateCard> -->
+    <!--   <a href="https://github.com/ht3aa/covid19"> -->
+    <!--     <div class="flex h-full"> -->
+    <!--       <LazyImg -->
+    <!--         extra-div-classes="md:w-1/3 w-[30%]" -->
+    <!--         extra-img-classes="object-cover h-full aspect-[0.6]" -->
+    <!--         extra-blurred-img-classes="object-cover h-full aspect-[0.6]" -->
+    <!--         blurred-img-src="/imgs/covid19-small.png" -->
+    <!--         main-img-src="/imgs/covid19.png" -->
+    <!--         alt="Covid 19" -->
+    <!--       /> -->
+    <!--       <div class="w-10/12 py-3 px-5 flex flex-col justify-center relative"> -->
+    <!--         <div -->
+    <!--           class="w-[3px] h-[90%] bgMainColor absolute left-1 top-[50%] translate-y-[-50%]" -->
+    <!--         /> -->
+    <!--         <h3 class="text-2xl">Covid 19</h3> -->
+    <!--         <p class="text-md"> -->
+    <!--           A project that contains a description about covid-19. also it -->
+    <!--           contains the feature of finding the nearest vaccine from you. -->
+    <!--         </p> -->
+    <!--       </div> -->
+    <!--     </div> -->
+    <!--   </a> -->
+    <!-- </RotateCard> -->
 
-    <RotateCard>
-      <a href="https://github.com/ht3aa/student_reg_system/tree/main">
-        <div class="flex h-full">
-          <LazyImg
-            extra-div-classes="md:w-1/3 w-[30%]"
-            extra-img-classes="object-cover h-full aspect-[0.6]"
-            extra-blurred-img-classes="object-cover h-full aspect-[0.6]"
-            blurred-img-src="/imgs/crud-small.png"
-            main-img-src="/imgs/crud.png"
-            alt="Crud Design"
-          />
-          <div class="w-10/12 py-3 px-5 flex flex-col justify-center relative">
-            <div
-              class="w-[3px] h-[90%] bgMainColor absolute left-1 top-[50%] translate-y-[-50%]"
-            />
-            <h3 class="text-2xl">Students Management System</h3>
-            <p class="text-md">
-              An integrated performance and quality students management system
-              for Imam Al-Kadhim College, Dhi Qar departments. Features: CRUD
-              functionality, Responsibility system, Access system, Simple Design
-            </p>
-          </div>
-        </div>
-      </a>
-    </RotateCard>
+    <!-- <RotateCard> -->
+    <!--   <a href="https://github.com/ht3aa/student_reg_system/tree/main"> -->
+    <!--     <div class="flex h-full"> -->
+    <!--       <LazyImg -->
+    <!--         extra-div-classes="md:w-1/3 w-[30%]" -->
+    <!--         extra-img-classes="object-cover h-full aspect-[0.6]" -->
+    <!--         extra-blurred-img-classes="object-cover h-full aspect-[0.6]" -->
+    <!--         blurred-img-src="/imgs/crud-small.png" -->
+    <!--         main-img-src="/imgs/crud.png" -->
+    <!--         alt="Crud Design" -->
+    <!--       /> -->
+    <!--       <div class="w-10/12 py-3 px-5 flex flex-col justify-center relative"> -->
+    <!--         <div -->
+    <!--           class="w-[3px] h-[90%] bgMainColor absolute left-1 top-[50%] translate-y-[-50%]" -->
+    <!--         /> -->
+    <!--         <h3 class="text-2xl">Students Management System</h3> -->
+    <!--         <p class="text-md"> -->
+    <!--           An integrated performance and quality students management system -->
+    <!--           for Imam Al-Kadhim College, Dhi Qar departments. Features: CRUD -->
+    <!--           functionality, Responsibility system, Access system, Simple Design -->
+    <!--         </p> -->
+    <!--       </div> -->
+    <!--     </div> -->
+    <!--   </a> -->
+    <!-- </RotateCard> -->
   </section>
 
   <section id="features" class="wrapper flex">
--- src/utils/global.js
+++ src/utils/global.js
@@ -1,4 +1,3 @@
-// 21
 export const customerOpinions = {
   columns1: [
     {
@@ -25,8 +24,7 @@ export const customerOpinions = {
 
     {
       name: "أنس اسكندر",
-      opinion:
-        "شخص مثابر ويحاول من تطوير نفسه ويحب مساعدة الآخرين بما يملك من علم..",
+      opinion: "شخص مثابر ويحاول من تطوير نفسه ويحب مساعدة الآخرين بما يملك من علم..",
     },
 
     { name: "فرقان فيصل", opinion: "محاضرة ممتازة" },
@@ -56,3 +54,67 @@ export const customerOpinions = {
     { name: "مريم العذراء رافد", opinion: "ممتاز من ناحية الشرح" },
   ],
 };
+
+export const projects = [
+  {
+    id: 1,
+    href: "#hero",
+    blurredImg: "/imgs/me-small.png",
+    mainImg: "/imgs/me.png",
+    imgAlt: "portfolio",
+    title: "Portfolio",
+    description:
+      "Contains information about me, big projects that I has done, and what I offer to you as a friend first and customer second.",
+  },
+  {
+    id: 2,
+    href: " https://matlab-bag.vercel.app/ ",
+    blurredImg: "/imgs/ali_falih-small.png",
+    mainImg: "/imgs/ali_falih.png",
+    imgAlt: "Ali Falih",
+    title: "Matlab Bag",
+    description:
+      "A website that contains files and videos which explain Matlab software easily and simply. Tools that I used: Nuxt.js React framework and Vercel For hosting",
+  },
+  {
+    id: 3,
+    href: "https://excel-bag.vercel.app/",
+    blurredImg: "/imgs/mohammed_salman-small.jpg",
+    mainImg: "/imgs/mohammed_salman.jpg",
+    imgAlt: "Mohammed Salman",
+    title: "Excel Bag",
+    description:
+      "A website that contains files and videos which explain Excel software easily and simply. Tools that I used: Nuxt.js React framework and Vercel For hosting",
+  },
+
+  {
+    id: 4,
+    href: "https://github.com/ht3aa/english_bag",
+    blurredImg: "/imgs/computer_teqniques_logo-small.png",
+    mainImg: "/imgs/computer_teqniques_logo.png",
+    imgAlt: "University Logo",
+    title: "English Bag",
+    description:
+      "A project that contains files and videos which explain English language easily and simply. features: Downloadable lecture files, voice records. You can read the lecture while listening the voice record or the videos responsive.",
+  },
+  {
+    id: 5,
+    href: "https://github.com/ht3aa/covid19",
+    blurredImg: "/imgs/covid19-small.png",
+    mainImg: "/imgs/covid19.png",
+    imgAlt: "Covid 19",
+    title: "Covid 19",
+    description:
+      "A project that contains a description about covid-19. also it contains the feature of finding the nearest vaccine from you.",
+  },
+  {
+    id: 6,
+    href: "https://github.com/ht3aa/student_reg_system/tree/main",
+    blurredImg: "/imgs/crud-small.png",
+    mainImg: "/imgs/crud.png",
+    imgAlt: "Crud Design",
+    title: "Students Management System",
+    description:
+      "An integrated performance and quality students management system for Imam Al-Kadhim College, Dhi Qar departments. Features: CRUD functionality, Responsibility system, Access system, Simple Design",
+  },
+];
