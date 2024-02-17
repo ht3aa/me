8fbe864808cd6b8635cdb8ddd2472d7bf05f012a
ht3aa <ht3aa2001@gmail.com>
2024-02-17 14:25:43 +0300

    Adding certificates section to navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -145,6 +145,30 @@ import MacAnimList from "./MacAnimList.vue";
       </a>
     </li>
     <li>
+      <a class="navItem relative" href="#certificates">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+          />
+        </svg>
+
+        <div
+          class="tooltip w-max absolute left-1/2 bottom-[-40px] translate-x-[-50%] bg-white textMainColor borderMainColor shadow-lg py-1 px-4"
+        >
+          Certificates
+        </div>
+      </a>
+    </li>
+    <li>
       <a class="navItem relative" href="#socialMedia">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -187,8 +211,8 @@ li svg {
   height: 30px;
 }
 
-
-li:hover, li:focus {
+li:hover,
+li:focus {
   cursor: pointer;
   color: #228be6;
 }
