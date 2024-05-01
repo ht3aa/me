35e771f4a5f1c8b420c972c28711677dda023832
ht3aa <ht3aa2001@gmail.com>
2024-05-01 23:26:02 +0300

    feat: adding new job :))))))))))

--- src/components/JobsCard.vue
+++ src/components/JobsCard.vue
@@ -2,12 +2,12 @@
 import LazyImg from "./LazyImg.vue";
 
 defineProps({
-  job: Object
-})
+  job: Object,
+});
 </script>
 
 <template>
-  <div class="borderMainColor text-center py-2">
+  <div class="borderMainColor text-center py-2 px-4">
     <LazyImg
       extra-img-classes="w-[150px] mx-auto rounded-full"
       :blurred-img-src="job.blurredImg"
@@ -15,11 +15,26 @@ defineProps({
       :alt="job.alt"
     />
     <h2 class="text-3xl md:text-4xl textMainColor">{{ job.companyName }}</h2>
-    <p class="my-3">
-      I worked their as <strong>"{{ job.position }}"</strong
-      >.
+    <p>
+      type of work: <strong>"{{ job.type }}"</strong>
     </p>
-    <em class="my-3"><small>{{ job.from }} - {{ job.to }}</small></em>
+    <p class="my-3">
+      I worked their as <strong>"{{ job.position }}"</strong>.
+    </p>
+
+    <div class="my-5 grid grid-cols-1 md:grid-cols-2 gap-2">
+      <div v-for="contribution in job.contributions">
+        <strong>"Contribution"</strong>
+        <p>Name: {{ contribution.name }}</p>
+        <p>Role: {{ contribution.role }}</p>
+        <p>
+          <a class="textMainColor" :href="contribution.link">{{ contribution.linkName }}</a>
+        </p>
+      </div>
+    </div>
+    <em class="my-3"
+      ><small>{{ job.from }} - {{ job.to }}</small></em
+    >
   </div>
 </template>
 
