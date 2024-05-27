685a068259e5d76a8591124235ac01d62b6c0c3c
ht3aa <ht3aa2001@gmail.com>
2024-05-27 22:46:57 +0300

    fix: add contanct info in banner.vue

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -9,7 +9,7 @@ const state = ref("success")
 <template>
 
   <div class="py-1 text-center w-full text-white" :class="state === 'error' ? 'bg-red-700' :  state === 'success' ?'bg-green-700' : 'bg-blue-700'">
-    <p>I'm currently online and seeks for a job</p>
+    <p>I'm currently online and seeks for a job. <a class="underline" href="https://t.me/ht3aa" >Telegram</a> - <a class="underline" href="https://github.com/ht3aa">Github</a> - <a class="underline" href="https://www.linkedin.com/in/hassan-tahseen-3a9b2b1b0/">Linkedin</a></p>
   </div>
 
 </template>
